56ccf031a4765fdd3927ed6e69b76862b3ab4128
jwvonk <jwavonk@gmail.com>
2023-11-20 18:17:30 -0800

    Grid Initial Progress

--- src/classes/grid.ts
+++ src/classes/grid.ts
@@ -1,3 +1,30 @@
+interface Position {
+    row: number,
+    col: number,
+}
 
+interface TileObject {
+    pos: Position;
+  }
 
-// grid map the game will be played on
+export default class Grid {
+  private static tiles = new Map<Position, TileObject>;
+  /**
+   * @param width the width of the grid in tiles
+   * @param height the height of the grid in tiles
+   */
+  constructor(readonly width: number, readonly height: number) {}
+
+  public static hasTileObj(pos: Position): boolean {
+    return Grid.tiles.has(pos);
+  }
+
+  public static getTileObj(pos: Position): TileObject {
+    return Grid.tiles.get(pos)!;
+  }
+
+  public static setTileObj(obj: TileObject) {
+    Grid.tiles.set(obj.pos, obj);
+  }
+
+}
