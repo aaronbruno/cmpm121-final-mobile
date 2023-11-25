4a569b921e624a083d603e11f94048d608073b5d
Anthony Umemoto <atumemot@ucsc.edu>
2023-11-25 14:21:39 -0800

    grid config white space

--- src/grid-config.ts
+++ src/grid-config.ts
@@ -1,19 +1,17 @@
-
-
 interface GridConfig {
-    readonly rows: number;
-    readonly columns: number;
-    readonly tileWidth: number;
-    readonly tileHeight: number;
-    readonly width: number;
-    readonly height: number;
+  readonly rows: number;
+  readonly columns: number;
+  readonly tileWidth: number;
+  readonly tileHeight: number;
+  readonly width: number;
+  readonly height: number;
 }
 
 export const gridConfig: GridConfig = {
-    rows: 10,
-    columns: 10,
-    tileWidth: 128,
-    tileHeight: 128,
-    height: 10 * 128,
-    width: 10 * 128
+  rows: 10,
+  columns: 10,
+  tileWidth: 128,
+  tileHeight: 128,
+  height: 10 * 128,
+  width: 10 * 128,
 };
