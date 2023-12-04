d0b063653ef780ffe5cd70e33becf9acd71b849c
Anthony Umemoto <atumemot@ucsc.edu>
2023-12-04 15:04:26 -0800

    getting images in build hopefully

---
+++ src/import-assets.ts
@@ -0,0 +1,36 @@
+
+// buttons
+import "../assets/buttons/greenButton.png";
+import "../assets/buttons/purpleButton.png";
+import "../assets/buttons/redButton.png";
+import "../assets/buttons/redoButton.png";
+import "../assets/buttons/redoButtonChinese.png";
+import "../assets/buttons/redoButtonHebrew.png";
+import "../assets/buttons/saveButton.png";
+import "../assets/buttons/saveButtonChinese.png";
+import "../assets/buttons/saveButtonHebrew.png";
+import "../assets/buttons/undoButton.png";
+import "../assets/buttons/undoButtonChinese.png";
+import "../assets/buttons/undoButtonHebrew.png";
+
+// characters
+//import "../assets/characters/blue.json";
+import "../assets/characters/blue.png";
+import "../assets/characters/blueFront.png";
+
+// plants
+import "../assets/plants/greenlevel1.png";
+import "../assets/plants/greenlevel2.png";
+import "../assets/plants/greenlevel3.png";
+import "../assets/plants/purplelevel1.png";
+import "../assets/plants/purplelevel2.png";
+import "../assets/plants/purplelevel3.png";
+import "../assets/plants/redlevel1.png";
+import "../assets/plants/redlevel2.png";
+import "../assets/plants/redlevel3.png";
+
+// terrain
+import "../assets/plants/drydirt.png";
+import "../assets/plants/farmDry.json";
+import "../assets/plants/farmtiles.png";
+import "../assets/plants/farmWet.json";
