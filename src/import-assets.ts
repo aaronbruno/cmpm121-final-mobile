cd92e144ce99b35b55fbcd024ba23e6d3de99797
Anthony Umemoto <atumemot@ucsc.edu>
2023-12-09 14:09:55 -0800

    tghdfsvdfbdgn

--- src/import-assets.ts
+++ src/import-assets.ts
@@ -1,63 +1,63 @@
 
 // buttons
-import greenButton from "./assets/buttons/greenButton.png";
+import greenButton from "./assets/buttons/greenButton.png?url";
 export { greenButton};
-import purpleButton from "./assets/buttons/purpleButton.png";
+import purpleButton from "./assets/buttons/purpleButton.png?url";
 export { purpleButton };
-import redButton from "./assets/buttons/redButton.png";
+import redButton from "./assets/buttons/redButton.png?url";
 export { redButton };
-import redoButton from "./assets/buttons/redoButton.png";
+import redoButton from "./assets/buttons/redoButton.png?url";
 export { redoButton };
-import redoButtonChinese from "./assets/buttons/redoButtonChinese.png";
+import redoButtonChinese from "./assets/buttons/redoButtonChinese.png?url";
 export { redoButtonChinese };
-import redoButtonHebrew from "./assets/buttons/redoButtonHebrew.png";
+import redoButtonHebrew from "./assets/buttons/redoButtonHebrew.png?url";
 export { redoButtonHebrew };
-import saveButton from "./assets/buttons/saveButton.png";
+import saveButton from "./assets/buttons/saveButton.png?url";
 export { saveButton };
-import saveButtonChinese from "./assets/buttons/saveButtonChinese.png";
+import saveButtonChinese from "./assets/buttons/saveButtonChinese.png?url";
 export { saveButtonChinese};
-import saveButtonHebrew from "./assets/buttons/saveButtonHebrew.png";
+import saveButtonHebrew from "./assets/buttons/saveButtonHebrew.png?url";
 export { saveButtonHebrew };
-import undoButton from "./assets/buttons/undoButton.png";
+import undoButton from "./assets/buttons/undoButton.png?url";
 export { undoButton };
-import undoButtonChinese from "./assets/buttons/undoButtonChinese.png";
+import undoButtonChinese from "./assets/buttons/undoButtonChinese.png?url";
 export { undoButtonChinese };
-import undoButtonHebrew from "./assets/buttons/undoButtonHebrew.png";
+import undoButtonHebrew from "./assets/buttons/undoButtonHebrew.png?url";
 export { undoButtonHebrew };
 
 // characters
 //import "./assets/characters/blue.json";
-import blue from "./assets/characters/blue.png";
+import blue from "./assets/characters/blue.png?url";
 export { blue };
-import blueFront from "./assets/characters/blueFront.png";
+import blueFront from "./assets/characters/blueFront.png?url";
 export { blueFront };
 
 // plants
-import greenLevel1 from "./assets/plants/greenlevel1.png";
+import greenLevel1 from "./assets/plants/greenlevel1.png?url";
 export { greenLevel1 };
-import greenLevel2 from "./assets/plants/greenlevel2.png";
+import greenLevel2 from "./assets/plants/greenlevel2.png?url";
 export { greenLevel2 };
-import greenLevel3 from "./assets/plants/greenlevel3.png";
+import greenLevel3 from "./assets/plants/greenlevel3.png?url";
 export { greenLevel3 };
-import purpleLevel1 from "./assets/plants/purplelevel1.png";
+import purpleLevel1 from "./assets/plants/purplelevel1.png?url";
 export { purpleLevel1 };
-import purpleLevel2 from "./assets/plants/purplelevel2.png";
+import purpleLevel2 from "./assets/plants/purplelevel2.png?url";
 export { purpleLevel2 };
-import purpleLevel3 from "./assets/plants/purplelevel3.png";
+import purpleLevel3 from "./assets/plants/purplelevel3.png?url";
 export { purpleLevel3 };
-import redLevel1 from "./assets/plants/redlevel1.png";
+import redLevel1 from "./assets/plants/redlevel1.png?url";
 export { redLevel1 };
-import redLevel2 from "./assets/plants/redlevel2.png";
+import redLevel2 from "./assets/plants/redlevel2.png?url";
 export { redLevel2 };
-import redLevel3 from "./assets/plants/redlevel3.png";
+import redLevel3 from "./assets/plants/redlevel3.png?url";
 export { redLevel3 };
 
 // terrain
-import dryDirt from "./assets/terrain/drydirt.png";
+import dryDirt from "./assets/terrain/drydirt.png?url";
 export { dryDirt };
 import farmDry from "./assets/terrain/farmDry.json";
 export { farmDry };
-import farmTiles from "./assets/terrain/farmtiles.png";
+import farmTiles from "./assets/terrain/farmtiles.png?url";
 export { farmTiles };
 import farmWet from "./assets/terrain/farmWet.json";
 export { farmWet };
