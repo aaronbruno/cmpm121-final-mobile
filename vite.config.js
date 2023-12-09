c38b8ab736217e96c55336294eb5cd8a3ffcbc66
Anthony Umemoto <atumemot@ucsc.edu>
2023-12-09 13:18:41 -0800

    kjhgcfgh

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ export default {
   // eslint-disable-next-line no-undef
   base: process.env.REPO_NAME || "/repo-name/",
   build: {
-    assetInlineLimit: "500"
+    assetInlineLimit: "50"
   }
 };
