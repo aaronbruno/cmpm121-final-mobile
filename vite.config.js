72191a08860af34567c2d3ff202cba8297245556
Anthony Umemoto <atumemot@ucsc.edu>
2023-12-09 13:33:05 -0800

    jhghjk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   // eslint-disable-next-line no-undef
   base: process.env.REPO_NAME || "/repo-name/",
   build: {
     assetInlineLimit: "50"
   }
-};
+});
