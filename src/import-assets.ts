7ee448180c23dbff27f4f30f0588665baf64ae44
Anthony Umemoto <atumemot@ucsc.edu>
2023-12-09 13:09:11 -0800

    please god work

--- src/import-assets.ts
+++ src/import-assets.ts
@@ -1,36 +1,63 @@
 
 // buttons
-import "../assets/buttons/greenButton.png";
-import "../assets/buttons/purpleButton.png";
-import "../assets/buttons/redButton.png";
-import "../assets/buttons/redoButton.png";
-import "../assets/buttons/redoButtonChinese.png";
-import "../assets/buttons/redoButtonHebrew.png";
-import "../assets/buttons/saveButton.png";
-import "../assets/buttons/saveButtonChinese.png";
-import "../assets/buttons/saveButtonHebrew.png";
-import "../assets/buttons/undoButton.png";
-import "../assets/buttons/undoButtonChinese.png";
-import "../assets/buttons/undoButtonHebrew.png";
+import greenButton from "../assets/buttons/greenButton.png";
+export { greenButton};
+import purpleButton from "../assets/buttons/purpleButton.png";
+export { purpleButton };
+import redButton from "../assets/buttons/redButton.png";
+export { redButton };
+import redoButton from "../assets/buttons/redoButton.png";
+export { redoButton };
+import redoButtonChinese from "../assets/buttons/redoButtonChinese.png";
+export { redoButtonChinese };
+import redoButtonHebrew from "../assets/buttons/redoButtonHebrew.png";
+export { redoButtonHebrew };
+import saveButton from "../assets/buttons/saveButton.png";
+export { saveButton };
+import saveButtonChinese from "../assets/buttons/saveButtonChinese.png";
+export { saveButtonChinese};
+import saveButtonHebrew from "../assets/buttons/saveButtonHebrew.png";
+export { saveButtonHebrew };
+import undoButton from "../assets/buttons/undoButton.png";
+export { undoButton };
+import undoButtonChinese from "../assets/buttons/undoButtonChinese.png";
+export { undoButtonChinese };
+import undoButtonHebrew from "../assets/buttons/undoButtonHebrew.png";
+export { undoButtonHebrew };
 
 // characters
 //import "../assets/characters/blue.json";
-import "../assets/characters/blue.png";
-import "../assets/characters/blueFront.png";
+import blue from "../assets/characters/blue.png";
+export { blue };
+import blueFront from "../assets/characters/blueFront.png";
+export { blueFront };
 
 // plants
-import "../assets/plants/greenlevel1.png";
-import "../assets/plants/greenlevel2.png";
-import "../assets/plants/greenlevel3.png";
-import "../assets/plants/purplelevel1.png";
-import "../assets/plants/purplelevel2.png";
-import "../assets/plants/purplelevel3.png";
-import "../assets/plants/redlevel1.png";
-import "../assets/plants/redlevel2.png";
-import "../assets/plants/redlevel3.png";
+import greenLevel1 from "../assets/plants/greenlevel1.png";
+export { greenLevel1 };
+import greenLevel2 from "../assets/plants/greenlevel2.png";
+export { greenLevel2 };
+import greenLevel3 from "../assets/plants/greenlevel3.png";
+export { greenLevel3 };
+import purpleLevel1 from "../assets/plants/purplelevel1.png";
+export { purpleLevel1 };
+import purpleLevel2 from "../assets/plants/purplelevel2.png";
+export { purpleLevel2 };
+import purpleLevel3 from "../assets/plants/purplelevel3.png";
+export { purpleLevel3 };
+import redLevel1 from "../assets/plants/redlevel1.png";
+export { redLevel1 };
+import redLevel2 from "../assets/plants/redlevel2.png";
+export { redLevel2 };
+import redLevel3 from "../assets/plants/redlevel3.png";
+export { redLevel3 };
 
 // terrain
-import "../assets/terrain/drydirt.png";
-import "../assets/terrain/farmDry.json";
-import "../assets/terrain/farmtiles.png";
-import "../assets/terrain/farmWet.json";
+import dryDirt from "../assets/terrain/drydirt.png";
+export { dryDirt };
+import farmDry from "../assets/terrain/farmDry.json";
+export { farmDry };
+import farmTiles from "../assets/terrain/farmtiles.png";
+export { farmTiles };
+import farmWet from "../assets/terrain/farmWet.json";
+export { farmWet };
